43d9b6e770a7df33838e913c9b7223acf2e13b5d
KartikPatelOfficial <patelkartik1910@gmail.com>
2019-03-18 22:14:28 +0530

    Refactor: Getting details from view model in account fragments.

--- app/src/main/java/com/deucate/pubgvictory/account/AccountFragment.kt
+++ app/src/main/java/com/deucate/pubgvictory/account/AccountFragment.kt
@@ -8,7 +8,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
@@ -16,24 +15,18 @@ import com.deucate.pubgvictory.MainViewModel
 import com.deucate.pubgvictory.R
 import com.deucate.pubgvictory.auth.LoginActivity
 import com.deucate.pubgvictory.model.User
-import com.deucate.pubgvictory.utils.Constatns
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_account.view.*
 import net.glxn.qrgen.android.QRCode
 
 
 class AccountFragment : Fragment() {
 
-    lateinit var viewModel: MainViewModel
+    private lateinit var viewModel: MainViewModel
 
     private lateinit var user: User
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.fragment_account, container, false)
 
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
@@ -43,7 +36,7 @@ class AccountFragment : Fragment() {
             updateUI(rootView)
         })
 
-        val qrCode = QRCode.from(user.ID).bitmap()
+        val qrCode = QRCode.from(user.ID).withSize(250,250).bitmap()
         rootView.qrCode.setImageBitmap(qrCode)
         rootView.authUID.text = user.ID
 
@@ -62,11 +55,10 @@ class AccountFragment : Fragment() {
 
     private fun updateUI(rootView: View) {
         rootView.accountName.text = user.Name
-        rootView.accountEmail.text = user.Email
         rootView.accountPhone.text = user.Phone
 
         Glide.with(this).load(FirebaseAuth.getInstance().currentUser!!.photoUrl)
-            .into(rootView.accountProfilePicture)
+                .into(rootView.accountProfilePicture)
     }
 
 
